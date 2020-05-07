ff2726e2bfde7a7fc1948cf5b09aead82cf464e8
Suyatna <zuyatna@gmail.com>
2020-05-07 14:27:21 +0700

    20 - Using Snippets

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,7 +8,18 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int customerId { get; private set; }
+        public string emailAddress { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
+        public string fullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+
     }
 }
