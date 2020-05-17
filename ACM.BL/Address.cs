d7cc56b425176e5aba9075f94abb564bfb70916a
Suyatna <zuyatna@gmail.com>
2020-05-17 14:32:18 +0700

    38 - Building the Address Class

---
+++ ACM.BL/Address.cs
@@ -0,0 +1,31 @@
+﻿namespace ACM.BL
+{
+    public class Address
+    {
+        public Address()
+        {
+            
+        }
+        
+        public Address(int addressId)
+        {
+            this.addressId = addressId;
+        }
+
+        public int addressId { get; private set; }
+        public int addressType { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string postalCode { get; set; }
+        public string state { get; set; }
+        public string streetLine1 { get; set; }
+        public string streetLine2 { get; set; }
+
+        public bool Validate()
+        {
+            bool isValid = postalCode != null;
+
+            return isValid;
+        }
+    }
+}
