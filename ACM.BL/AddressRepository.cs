12c1fc8176fb3ad19ce701868eaebc0fb542d50c
Suyatna <zuyatna@gmail.com>
2020-06-09 20:38:45 +0700

    49 - Populating the Referenced Objects

---
+++ ACM.BL/AddressRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class AddressRepository
+    {
+        public Address Retrieve(int addressId)
+        {
+            Address address = new Address();
+            
+            // temporary hard-coded value to return
+            // a populated address
+            if (addressId == 1)
+            {
+                address.addressType = 1;
+                address.streetLine1 = "Tubagus Ismail Bawah";
+                address.streetLine2 = "Ir. H. Djuanda";
+                address.city = "Bandung";
+                address.state = "West Java";
+                address.country = "Indonesia";
+                address.postalCode = "1425";
+            }
+
+            return address;
+        }
+
+        public IEnumerable<Address> RetrieveByCustomerId(int customerId)
+        {
+            // temporary hard-coded value to return
+            // a set of address for a customer
+            var addressList = new List<Address>();
+            Address address = new Address(1)
+            {
+                addressType = 1,
+                streetLine1 = "Tubagus Ismail Bawah",
+                streetLine2 = "Ir. H. Djuanda",
+                city = "Bandung",
+                state = "West Java",
+                country = "Indonesia",
+                postalCode = "1425"
+            };
+            addressList.Add(address);
+            
+            address = new Address(2)
+            {
+                addressType = 2,
+                streetLine1 = "Jl. Pemuda",
+                city = "Bekasi",
+                state = "West Java",
+                country = "Indonesia",
+                postalCode = "1752"
+            };
+            addressList.Add(address);
+
+            return addressList;
+        }
+        
+        public bool Save(Address address)
+        {
+            return true;
+        }
+    }
+}
--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -1,16 +1,28 @@
-﻿namespace ACM.BL
+﻿using System.Linq;
+
+namespace ACM.BL
 {
     public class CustomerRepository
     {
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
+        
+        private AddressRepository addressRepository { get; set; }
+        
         public Customer Retrieve(int customerId)
         {
             Customer customer = new Customer(customerId);
 
+            // temporary hard-coded value to return
+            // a populated customer
             if (customerId == 1)
             {
                 customer.emailAddress = "zuyatna@gmail.com";
                 customer.firstName = "Suyatna";
                 customer.lastName = "Light";
+                customer.addressList = addressRepository.RetrieveByCustomerId(customerId).ToList();
             }
 
             return customer;
