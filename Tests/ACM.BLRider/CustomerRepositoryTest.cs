875f33ab81fbccb0dd38f531796c17dc535b837a
Suyatna <zuyatna@gmail.com>
2020-07-24 11:17:22 +0700

    50 - Testing a Composition Relationship

--- Tests/ACM.BLRider/CustomerRepositoryTest.cs
+++ Tests/ACM.BLRider/CustomerRepositoryTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ACM.BL;
 using Xunit;
 
@@ -24,6 +25,58 @@ namespace ACM.BLRider
             Assert.Equal(expected.emailAddress, actual.emailAddress);
             Assert.Equal(expected.firstName, actual.firstName);
             Assert.Equal(expected.lastName, actual.lastName);
+        }
+
+        [Fact]
+        public void RetrieveExistingWithAddress()
+        {
+            var customerRepository = new CustomerRepository();
+            
+            var expected = new Customer(1)
+            {
+                emailAddress = "zuyatna@gmail.com",
+                firstName = "Suyatna",
+                lastName = "Light",
+                addressList = new List<Address>()
+                {
+                    new Address()
+                    {
+                        addressType = 1,
+                        streetLine1 = "Tubagus Ismail Bawah",
+                        streetLine2 = "Ir. H. Djuanda",
+                        city = "Bandung",
+                        state = "West Java",
+                        country = "Indonesia",
+                        postalCode = "1425"
+                    },
+                    new Address()
+                    {
+                        addressType = 2,
+                        streetLine1 = "Jl. Pemuda",
+                        city = "Bekasi",
+                        state = "West Java",
+                        country = "Indonesia",
+                        postalCode = "1752"
+                    }
+                }
+            };
+
+            var actual = customerRepository.Retrieve(1);
+            
+            Assert.Equal(expected.customerId, actual.customerId);
+            Assert.Equal(expected.emailAddress, actual.emailAddress);
+            Assert.Equal(expected.firstName, actual.firstName);
+            Assert.Equal(expected.lastName, actual.lastName);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.Equal(expected.addressList[i].addressType, actual.addressList[i].addressType);
+                Assert.Equal(expected.addressList[i].streetLine1, actual.addressList[i].streetLine1);
+                Assert.Equal(expected.addressList[i].city, actual.addressList[i].city);
+                Assert.Equal(expected.addressList[i].state, actual.addressList[i].state);
+                Assert.Equal(expected.addressList[i].country, actual.addressList[i].country);
+                Assert.Equal(expected.addressList[i].postalCode, actual.addressList[i].postalCode);
+            }
         }
     }
 }
